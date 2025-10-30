826fe750ae56a219a76f2fbbf8b4f9b0c1c92f38
Antonio Goncalves <antonio.goncalves@gmail.com>
2025-10-30 17:21:45 +0100

    Mockito

--- portfolio/src/test/java/com/pluralsight/currencyexchange/portfolio/PortfolioServiceTest.java
+++ portfolio/src/test/java/com/pluralsight/currencyexchange/portfolio/PortfolioServiceTest.java
@@ -2,15 +2,18 @@ package com.pluralsight.currencyexchange.portfolio;
 
 import com.pluralsight.currencyexchange.currency.CurrencyRate;
 import com.pluralsight.currencyexchange.portfolio.trade.Trade;
+import io.quarkus.test.junit.QuarkusMock;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mockito;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -21,8 +24,33 @@ class PortfolioServiceTest {
   @Inject
   PortfolioService portfolioService;
 
+  @BeforeEach
+  void mock() {
+    PortfolioService mock = Mockito.mock(PortfolioService.class);
+
+    Mockito.when(mock.getAllCurrentRates()).thenReturn(List.of(
+      CurrencyRate.newBuilder().setCurrencyCode("AUD").setRate(1.5234).build(),
+      CurrencyRate.newBuilder().setCurrencyCode("CAD").setRate(1.3425).build(),
+      CurrencyRate.newBuilder().setCurrencyCode("CHF").setRate(1.3425).build(),
+      CurrencyRate.newBuilder().setCurrencyCode("EUR").setRate(0.9217).build(),
+      CurrencyRate.newBuilder().setCurrencyCode("GBP").setRate(0.7905).build(),
+      CurrencyRate.newBuilder().setCurrencyCode("JPY").setRate(149.25).build()
+    ));
+
+    Mockito.when(mock.getCurrentRate("AUD")).thenReturn(CurrencyRate.newBuilder().setCurrencyCode("AUD").setRate(1.5234).build());
+    Mockito.when(mock.getCurrentRate("CAD")).thenReturn(CurrencyRate.newBuilder().setCurrencyCode("CAD").setRate(1.3425).build());
+    Mockito.when(mock.getCurrentRate("CHF")).thenReturn(CurrencyRate.newBuilder().setCurrencyCode("CHF").setRate(1.3425).build());
+    Mockito.when(mock.getCurrentRate("EUR")).thenReturn(CurrencyRate.newBuilder().setCurrencyCode("EUR").setRate(0.9217).build());
+    Mockito.when(mock.getCurrentRate("GBP")).thenReturn(CurrencyRate.newBuilder().setCurrencyCode("GBP").setRate(0.7905).build());
+    Mockito.when(mock.getCurrentRate("JPY")).thenReturn(CurrencyRate.newBuilder().setCurrencyCode("JPY").setRate(149.25).build());
+
+    Mockito.when(mock.getAllTrades("user456")).thenReturn(List.of(new Trade("user456", BigDecimal.valueOf(100), "EUR", new BigDecimal("92.00"), BigDecimal.valueOf(0.92))));
+
+    QuarkusMock.installMockForType(mock, PortfolioService.class);
+  }
+
   @Test
-  void shouldGetCurrentRates() throws Exception {
+  void shouldGetAllCurrentRates() throws Exception {
     List<CurrencyRate> currencyRates = portfolioService.getAllCurrentRates();
 
     assertNotNull(currencyRates);
@@ -32,7 +60,7 @@ class PortfolioServiceTest {
 
   @ParameterizedTest
   @ValueSource(strings = {"AUD", "CAD", "CHF", "EUR", "GBP", "JPY"})
-  void shouldGetCurrentRate(String currencyCode) throws Exception {
+  void shouldGetCurrentRate(String currencyCode) {
     CurrencyRate currencyRate = portfolioService.getCurrentRate(currencyCode);
 
     assertNotNull(currencyRate);
@@ -54,10 +82,11 @@ class PortfolioServiceTest {
     assertFalse(trades.isEmpty());
     assertEquals("user456", trades.getFirst().userId());
     assertEquals("EUR", trades.getFirst().toCurrency());
-    assertEquals(100, trades.getFirst().usdAmount());
-    assertEquals(0.92f, trades.getFirst().exchangeRate());
-    assertEquals(92.00f, trades.getFirst().convertedAmount());
-    assertEquals("COMPLETED", trades.getFirst().status());
+    assertEquals(new BigDecimal(100), trades.getFirst().usdAmount());
+    assertEquals(new BigDecimal("0.92"), trades.getFirst().exchangeRate());
+    assertEquals(new BigDecimal("92.00"), trades.getFirst().convertedAmount());
+    String status = trades.getFirst().status();
+    assertTrue(status.equals("COMPLETED") || status.equals("CREATED"));
     assertNotNull(trades.getFirst().timestamp());
   }
 }
